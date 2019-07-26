71e4345bb0faf6622a52741e5b714d9e055099d1
AlexisRamirezHernandez <37453654+AlexisRamirezHernandez@users.noreply.github.com>
2019-07-25 21:19:03 -0500

    Update EditarMaquinaria.java

--- EditarMaquinaria.java
+++ EditarMaquinaria.java
@@ -1,4 +1,3 @@
-
 import java.awt.*;
 
 import javax.swing.*;
@@ -8,6 +7,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.imageio.ImageIO;
 
 //import componentetextfield.CampoDato;
 import javax.swing.ImageIcon;
@@ -17,18 +21,29 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 import javax.swing.border.Border;
 
 public class EditarMaquinaria extends JFrame {
 
-    public boolean confirmar;
-
-    EditarMaquinaria(String nombre, String tipo, int modelo, String estado, double precioRenta) {
+    //PrincipalOriginal pp = new PrincipalOriginal();
+    static CampoDato dPrecioRenta;
+    static JTextField MatriculaTxtEditar;
+    static CampoDato CostoTxtEditar;
+    static CampoDato ModelotxtEditar;
+    static JTextField NombreMaquinatxtEditar;
+    static JTextField marca_Maquinaria;
+    FileInputStream fis;
+    int longitud_bytes;
+    PreparedStatement psd;
+    ModeloTabla_Maquinaria modelo=new ModeloTabla_Maquinaria("Adminn", "admin");
+    
+    public EditarMaquinaria(int id) {
 
         setSize(860, 500);
         setTitle("Editar maquinarias");
         setResizable(false);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        //setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setVisible(true);
 
         JPanel DatosMaquinaria = new JPanel();
@@ -38,10 +53,11 @@ public class EditarMaquinaria extends JFrame {
         DatosMaquinaria.setBackground(Color.black);
 
         ///En este panel se deben poner la imagenes
-        JPanel Imagen = new JPanel();
+        JLabel Imagen = new JLabel();
         Imagen.setLayout(null);
         Imagen.setBounds(590, 93, 250, 230);
         Imagen.setBackground(Color.decode("#049cff"));
+        Imagen.setIcon(modelo.getImagen(id));
         DatosMaquinaria.add(Imagen);
 
         JLabel Encabezado = new JLabel("Editar datos de la maquinaría:");
@@ -58,7 +74,7 @@ public class EditarMaquinaria extends JFrame {
         NombreMaquinaEditar.setBounds(0, 30, 300, 150);
         DatosMaquinaria.add(NombreMaquinaEditar);
 
-        JTextField NombreMaquinatxtEditar = new JTextField(nombre);
+        NombreMaquinatxtEditar = new JTextField();
         NombreMaquinatxtEditar.setForeground(Color.black);
         NombreMaquinatxtEditar.setBorder(null);
         NombreMaquinatxtEditar.setBounds(163, 92, 200, 30);
@@ -72,7 +88,7 @@ public class EditarMaquinaria extends JFrame {
         DatosMaquinaria.add(ModeloEditar);
 
         //JTextField ModelotxtEditar = new JTextField(String.valueOf(modelo));        
-        CampoDato ModelotxtEditar = new CampoDato(String.valueOf(modelo));
+        ModelotxtEditar = new CampoDato();
         ModelotxtEditar.setForeground(Color.black);
         ModelotxtEditar.setBorder(null);
         ModelotxtEditar.setBounds(164, 140, 200, 30);
@@ -87,7 +103,8 @@ public class EditarMaquinaria extends JFrame {
         TipoEditar.setBounds(0, 125, 300, 150);
         DatosMaquinaria.add(TipoEditar);
 
-        JComboBox TipoComboEditar = new JComboBox();
+        String[] tiposMaquinas = {"Excavadora", "Revolvedora", "Volteo"};
+        JComboBox TipoComboEditar = new JComboBox(tiposMaquinas);
         TipoComboEditar.setForeground(Color.black);
         TipoComboEditar.setBorder(null);
         TipoComboEditar.setBounds(161, 190, 200, 30);
@@ -100,10 +117,10 @@ public class EditarMaquinaria extends JFrame {
         CostoEditar.setBounds(0, 170, 300, 150);
         DatosMaquinaria.add(CostoEditar);
 
-        CampoDato CostoTxtEditar=new CampoDato(String.valueOf(precioRenta));
+        CostoTxtEditar = new CampoDato();
         CostoTxtEditar.setForeground(Color.black);
         CostoTxtEditar.setBorder(null);
-        CostoTxtEditar.setBounds(161, 238, 200, 30);        
+        CostoTxtEditar.setBounds(161, 238, 200, 30);
         CostoTxtEditar.setTipo('D');
         CostoTxtEditar.setLongitud(15);
         DatosMaquinaria.add(CostoTxtEditar);
@@ -115,25 +132,26 @@ public class EditarMaquinaria extends JFrame {
         MatriculaEditar.setBounds(0, 220, 300, 150);
         DatosMaquinaria.add(MatriculaEditar);
 
-        JTextField MatriculaTxtEditar = new JTextField();
+        MatriculaTxtEditar = new JTextField();
         MatriculaTxtEditar.setForeground(Color.black);
         MatriculaTxtEditar.setBorder(null);
         MatriculaTxtEditar.setBounds(161, 290, 200, 30);
         DatosMaquinaria.add(MatriculaTxtEditar);
 
-        JLabel MarcaEditar = new JLabel("Marca:");
+        JLabel MarcaEditar = new JLabel("Precio Renta:");
         MarcaEditar.setForeground(Color.white);
         Font fuenteMarca = new Font("Arial", Font.BOLD, 14);
         MarcaEditar.setFont(fuenteMarca);
         MarcaEditar.setBounds(0, 270, 300, 150);
         DatosMaquinaria.add(MarcaEditar);
 
-        JTextField MarcaTxtEditar = new JTextField();
-        MarcaTxtEditar.setForeground(Color.black);
-        MarcaTxtEditar.setBorder(null);
-        MarcaTxtEditar.setBounds(161, 334, 200, 30);
-        DatosMaquinaria.add(MarcaTxtEditar);
-
+        CampoDato RentaTxtEditar = new CampoDato();
+        RentaTxtEditar.setForeground(Color.black);
+        RentaTxtEditar.setBorder(null);
+        RentaTxtEditar.setBounds(161, 334, 200, 30);
+        RentaTxtEditar.setTipo('D');
+        RentaTxtEditar.setLongitud(12);
+        DatosMaquinaria.add(RentaTxtEditar);
         // En esta parte van los estados, los cuales deben ser : EN USO, DISPONIBLE y MANTENIMIENTO
         JLabel EstadoMaquinaAgregar = new JLabel("Estado:");
         EstadoMaquinaAgregar.setForeground(Color.white);
@@ -148,11 +166,6 @@ public class EditarMaquinaria extends JFrame {
         EstadoMaquinatxtAgregar.setBorder(null);
         EstadoMaquinatxtAgregar.setBounds(427, 92, 150, 30);
         DatosMaquinaria.add(EstadoMaquinatxtAgregar);
-        for (int i = 0; i < tiposEstados.length; i++) {
-            if (tiposEstados[i].equals(estado)) {
-                EstadoMaquinatxtAgregar.setSelectedIndex(i);
-            }
-        }
 
         ///////Botones
         JButton AgregarMaquinaria = new JButton("Agregar maquinaría");
@@ -165,15 +178,51 @@ public class EditarMaquinaria extends JFrame {
         AgregarMaquinaria.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                new OpcionesEditar();
-                /*JFrame auxiliar = new AdvertenciaModificar(copy);
-                if (confirmar == true) {
+                //dispose();
+                String nombre = NombreMaquinatxtEditar.getText();
+                int modelo_maq = Integer.parseInt(ModelotxtEditar.getText());
+                String tipo = (String) TipoComboEditar.getSelectedItem();
+                double costo = Integer.parseInt(CostoTxtEditar.getText());
+                String matricula = MatriculaTxtEditar.getText();
+                //String marca = MarcaTxt.getText();
+                String estado = EstadoMaquinatxtAgregar.getSelectedItem().toString();
+                double precio_renta = Double.parseDouble(dPrecioRenta.getText());
+                ModeloTabla_Maquinaria m =new ModeloTabla_Maquinaria("Adminn","admin");
+                m.actualizaEstatus(id);
+                System.out.println(nombre);
+                try {
+                    Connection cn;
+                    cn =Clase_Conexion.getConexion();
+                    psd = cn.prepareStatement("UPDATE MAQUINARIA SET NOMBRE_MAQ=?,TIPO_MAQ=?,MODELO_MAQ=?,COSTO_MAQ=?,ESTADO_MAQ=?,PRECIORENTA_MAQ=?,MATRICULA_MAQ=?,IMAGEN_MAQ=? WHERE CLAVE_MAQ= "+id);
+                    psd.setString(1, nombre);
+                    psd.setString(2, tipo);
+                    psd.setInt(3, modelo_maq);
+                    psd.setDouble(4, costo);
+                    psd.setString(5, estado);
+                    psd.setDouble(6, precio_renta);
+                    psd.setString(7, matricula);
+                    
+                    psd.setBlob(8, fis, getLongitud());
+                    int res = psd.executeUpdate();
+                    
+                   if (res < 0) {
+                        JOptionPane.showMessageDialog(null, "No se pudo añadir el registro");
+                    }
+                    JOptionPane.showMessageDialog(null, "Registro Exitoso");
+                    cn.close();
+                    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    modelo.actualizaEstatus();
+                } catch (SQLException e) {
+                    System.err.println("Error en: " + e);
+                }
 
-                } else {
-
-                }*/
-
+                //modelo.actualizaEstatus();
+                //dispose();
             }
+
+                
+
+            
         });
 
         JButton AgregarFoto = new JButton("Agregar foto");
@@ -182,6 +231,27 @@ public class EditarMaquinaria extends JFrame {
         AgregarFoto.setBorder(new ComponenteBotonRedondo(40));
         AgregarFoto.setForeground(Color.white);
         DatosMaquinaria.add(AgregarFoto);
+        AgregarFoto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser jfc = new JFileChooser();
+                jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                int estado = jfc.showOpenDialog(null);
+                if (estado == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        fis = new FileInputStream(jfc.getSelectedFile());
+                        longitud_bytes = (int) jfc.getSelectedFile().length();
+
+                        Image icono = ImageIO.read(jfc.getSelectedFile()).getScaledInstance(Imagen.getWidth(), Imagen.getHeight(), Image.SCALE_DEFAULT);
+                        Imagen.setIcon(new ImageIcon(icono));
+                        Imagen.updateUI();
+
+                        setLongitud(longitud_bytes);
+                    } catch (Exception ee) {
+                    }
+                }
+            }
+        });
 
         JLabel background = new JLabel();
         background.add(DatosMaquinaria);
@@ -189,13 +259,11 @@ public class EditarMaquinaria extends JFrame {
         setVisible(true);
 
     }
+public void setLongitud(int longi){
+        longitud_bytes=longi;
+    }
+    public int getLongitud(){
+        return longitud_bytes;
+    }
 
-    /*
-    public static void main(String[] args) {
-        new EditarMaquinaria();
-    }
-     */
-    public void setConfirmar(boolean confirmar) {
-        this.confirmar = confirmar;
-    }
 }
